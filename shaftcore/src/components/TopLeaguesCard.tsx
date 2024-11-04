a5d4a88cbbba1c4d280d70b1327dffc3d3f626d4
robertorf077 <joseroberto@moki.com.br>
2024-11-04 17:20:10 -0300

    Plau componente

--- shaftcore/src/components/TopLeaguesCard.tsx
+++ shaftcore/src/components/TopLeaguesCard.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
+import Time from "../../public/it.png";
+import TimeBr from "../../public/br.png";
+import Image from "next/image";
 
 const TopLeaguesCard: React.FC = () => {
   const leagues = [
-    { name: 'Brasileirão Série A', icon: '/icons/brasileirao.png' },
-    { name: 'UEFA Champions League', icon: '/icons/champions_league.png' },
-    { name: 'UEFA Europa League', icon: '/icons/europa_league.png' },
-    { name: 'Premier League', icon: '/icons/premier_league.png' },
-    { name: 'LaLiga', icon: '/icons/laliga.png' },
-    { name: 'Bundesliga', icon: '/icons/bundesliga.png' },
-    { name: 'Serie A', icon: '/icons/serie_a.png' },
-    { name: 'UEFA Conference League', icon: '/icons/conference_league.png' },
-    { name: 'Ligue 1', icon: '/icons/ligue_1.png' },
-    { name: 'CONMEBOL Libertadores', icon: '/icons/libertadores.png' },
-    { name: 'World Championship', icon: '/icons/world_championship.png' },
+    { name: "Brasileirão Série A", icon: "/icons/brasileirao.png" },
+    { name: "UEFA Champions League", icon: "/icons/champions_league.png" },
+    { name: "UEFA Europa League", icon: "/icons/europa_league.png" },
+    { name: "Premier League", icon: "/icons/premier_league.png" },
+    { name: "LaLiga", icon: "/icons/laliga.png" },
+    { name: "Bundesliga", icon: "/icons/bundesliga.png" },
+    { name: "Serie A", icon: "/icons/serie_a.png" },
+    { name: "UEFA Conference League", icon: "/icons/conference_league.png" },
+    { name: "Ligue 1", icon: "/icons/ligue_1.png" },
+    { name: "CONMEBOL Libertadores", icon: "/icons/libertadores.png" },
+    { name: "World Championship", icon: "/icons/world_championship.png" },
   ];
 
   return (
     <div className="p-4 bg-white rounded-lg shadow-md mt-4">
-      <h2 className="text-lg font-bold mb-4">Top leagues</h2>
+      <h2 className="text-lg font-bold mb-4">Principais ligas</h2>
       <ul className="space-y-3">
         {leagues.map((league) => (
           <li key={league.name} className="flex items-center">
-            <img src={league.icon} alt={`${league.name} Icon`} className="w-6 h-6 mr-3" />
+            <Image src={TimeBr} alt="Team 1" className="w-6 h-6 mr-3" />
             <span className="font-medium">{league.name}</span>
           </li>
         ))}
